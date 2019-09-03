972aa06262fddf05b6b9f846736c4d1d3cf93300
Dalton Newport <Dalton.e.newport-1@ou.edu>
2019-09-03 15:49:05 -0500

    Good basic class example

---
+++ 9.3.19InClass/ConsoleApp1/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Computer
+    {
+        public double HardDriveSize { get; set;}
+        public double CPUSpeed { get; set;}
+        public int CPUCores { get; set;}
+        public string brand { get; set;}
+        public bool IsOverClocked { get; set;}
+
+        public Computer ()
+        {
+            HardDriveSize = 0;
+            CPUSpeed = 0;
+            CPUCores = 0;
+            brand = string.Empty;
+            IsOverClocked = false;
+        }
+
+        public double CalculatePrice ()
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ 9.3.19InClass/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+
+            names.Add("Dalton");
+            names.Add("Emily");
+            names.Add("Kayci");
+            
+            /* standard for loop
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            */
+
+            foreach (var currName in names)
+            {
+                Console.WriteLine(currName);
+            }
+
+
+            Computer myDesktop = new Computer();
+            myDesktop.brand = "ASUS";
+            myDesktop.CPUCores = 4;
+            myDesktop.CPUSpeed = 3.9;
+            myDesktop.HardDriveSize = 1280;
+            myDesktop.IsOverClocked = true;
+
+            Console.WriteLine(myDesktop.brand);
+
+
+            Console.ReadKey();
+        }
+
+        static void Concat(string wordOne, string wordTwo)
+        {
+            Console.WriteLine(wordOne + wordTwo);
+        }
+    }
+}
