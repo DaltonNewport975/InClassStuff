2b5f1c654a406204dc4f5247b7248799de200860
Dalton Newport <Dalton.e.newport-1@ou.edu>
2019-09-10 15:13:46 -0500

    Just found out I dont need to do this stuff so saving here

---
+++ 9.10.19InClass/MaybeGradedParticipationOnClasses/Course.cs
@@ -0,0 +1,33 @@
+﻿//@author Dalton Newport
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaybeGradedParticipationOnClasses
+{
+    class Course
+    {
+
+        public int CourseID { get; set; }
+        public int CourseNumber { get; set; }
+        public string CourseName { get; set; }
+        public string Subject { get; set; }
+        public string Instructor { get; set; }
+
+        public Course()
+        {
+            CourseID = 0;
+            CourseNumber = 0;
+            CourseName = string.Empty;
+            Subject = string.Empty;
+            Instructor = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"\nCourse ID: {CourseID}\nCourse Number: {CourseNumber}\nCourse Name: {CourseName}\nSubject: {Subject}\nInstructor: {Instructor}";
+        }
+    }
+}
