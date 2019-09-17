7fca201a68277d1b0c2abc918db9b31f236100fa
Dalton Newport <Dalton.e.newport-1@ou.edu>
2019-09-17 15:43:46 -0500

    Connected to a database and did some things

---
+++ 9.17.19InClass/DatabaseConnection/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseConnection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNRun_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Student student in db.Students)
+            {
+                string temp = $"{student.FirstName}, {student.LastName}, {student.StudentID}, {student.FavoriteColor}";
+                LSBData.Items.Add(temp);
+            }
+        }
+    }
+}
